541c55c4f21cc6830930ff0e4e3465358b4e5794
fatma <fatmaramadan28@gmail.com>
2017-02-14 14:44:56 +0200

    ServerChat Implementation

--- InterfaceChatVC/src/interfacechatvc/ClientInt.java
+++ InterfaceChatVC/src/interfacechatvc/ClientInt.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package serverchatvc;
-
-import java.rmi.server.UnicastRemoteObject;
+package interfacechatvc;
 
 /**
  *
  * @author fatma
  */
-public class serverImp {
-   
+public class ClientInt {
+    
 }
